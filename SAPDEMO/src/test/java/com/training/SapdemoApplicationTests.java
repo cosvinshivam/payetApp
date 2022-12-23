6fd58010f0b70bc65fb0fb222eda2335ef7f69bc
shivam <cosvinshivam@gmail.com>
2022-12-23 17:25:41 +0100

    done

--- SAPDEMO/src/test/java/com/training/SapdemoApplicationTests.java
+++ SAPDEMO/src/test/java/com/training/SapdemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.training;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PaynetApplicationTests {
+class SapdemoApplicationTests {
 
 	@Test
 	void contextLoads() {
